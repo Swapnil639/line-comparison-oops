82323d9f69b8d2e8d53af752b1f4a1552858f753
Swapnil639 <swapnilpatil631997@gmail.com>
2022-06-04 01:13:47 +0530

    Added :Compare Methods using OOPs

--- src/com/bridgelabz/oops/LineComparisonUC4.java
+++ src/com/bridgelabz/oops/LineComparisonUC4.java
@@ -2,10 +2,34 @@ package com.bridgelabz.oops;
 
 import java.util.Scanner;
 
-public class LineComparisonUC3 {
-    static void compareLine() {
+public class LineComparisonUC4 {
+    static int x1, x2, y1, y2;
+    static void equalsTo() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter First Line Input :");
+        x1 = sc.nextInt();
+        x2 = sc.nextInt();
+        y1 = sc.nextInt();
+        y2 = sc.nextInt();
 
-        int x1, x2, y1, y2;
+        double length1 = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+        System.out.println("Length of Line1: " + length1);
+
+        System.out.print("Enter Second Line Input :");
+        x1 = sc.nextInt();
+        x2 = sc.nextInt();
+        y1 = sc.nextInt();
+        y2 = sc.nextInt();
+
+        double length2 = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+        System.out.println("Length of Line2: " + length2);
+
+        if (length1 == length2)
+            System.out.println("Line1 and Line2 are equal");
+        else
+            System.out.println("Line1 and Line2 are not equal");
+    }
+    static void compareTo() {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter First Line Input :");
         x1 = sc.nextInt();
@@ -34,8 +58,8 @@ public class LineComparisonUC3 {
     }
 
     public static void main(String[] args) {
-        compareLine();
+        equalsTo();
+        compareTo();
     }
+
 }
-
-
